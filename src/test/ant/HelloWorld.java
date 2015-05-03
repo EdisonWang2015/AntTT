a001b7dc330495c8343af45fcdc393c9b1b6211e
EdisonWang2015 <EdisonWang2015@users.noreply.github.com>
2015-05-03 23:07:52 +0800

    hello

--- src/test/ant/HelloWorld.java
+++ src/test/ant/HelloWorld.java
@@ -1,6 +1,6 @@
 package test.ant;  
 public class HelloWorld {  
-    public static v main(String[] args) {  
+    public static main(String[] args) {  
         // TODO Auto-generated method stub  
         System.out.println("HelloWord !!");  
     }  
