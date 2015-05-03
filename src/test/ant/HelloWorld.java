97fa8576c0364c01d062bba7e84519fa5d46dc1f
EdisonWang2015 <EdisonWang2015@users.noreply.github.com>
2015-05-03 22:44:20 +0800

    ssadf
    sdfasf

--- src/test/ant/HelloWorld.java
+++ src/test/ant/HelloWorld.java
@@ -1,6 +1,6 @@
 package test.ant;  
 public class HelloWorld {  
-    public static vo main(String[] args) {  
+    public static v main(String[] args) {  
         // TODO Auto-generated method stub  
         System.out.println("HelloWord !!");  
     }  
